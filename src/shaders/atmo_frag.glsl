a7481a6a1a414d1bab7e558ce4e25ff2a7b2148d
nenko <devnenko@gmail.com>
2024-02-13 13:50:00 +0100

    improved atmosphere shader readibility

--- src/shaders/atmo_frag.glsl
+++ src/shaders/atmo_frag.glsl
@@ -19,17 +19,17 @@ void main() {
     float lightIntensity = dot(vWorldNormal, vec3(cos(sunAngle), 0.0, sin(sunAngle)));
     float atmosphereMask = clamp(lightIntensity + 0.3, 0.25, 1.0);
 
-    //fresnle effect
-    float fresnleAmount = 5.0;
-    float fresnleSpread=0.6;
-    float fresnle = pow(1.2 - dot(vNormal, vec3(0.0, 0.0, 1.0)), fresnleAmount) * fresnleAmount * fresnleSpread;
+    //fresnel effect
+    float fresnelAmount = 5.0;
+    float fresnelSpread=0.6;
+    float fresnel = pow(1.2 - dot(vNormal, vec3(0.0, 0.0, 1.0)), fresnelAmount) * fresnelAmount * fresnelSpread;
 
     //clouds
     float clouds = texture2D(uClouds, vUv).x;
 
     //combine everything to atmosphere
     vec3 atmoColor = vec3(0.6, 0.8, 1.5);
-    vec3 atmoShaded = flattenVec3(atmosphereMask * (fresnle + clouds) * atmoColor);
+    vec3 atmoShaded = flattenVec3(atmosphereMask * (fresnel + clouds) * atmoColor);
 
     gl_FragColor = vec4(atmoShaded.xyz, atmoShaded.x);
 }
